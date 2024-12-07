55af254ce26916286a578960aadcc25d9fdba8f7
padnom <padnomdev@gmail.com>
2024-12-07 10:35:06 +0100

    feat:day02

--- exercise/C#/day02/Games.Tests/FizzBuzzTests.cs
+++ exercise/C#/day02/Games.Tests/FizzBuzzTests.cs
@@ -8,23 +8,38 @@ namespace Games.Tests
 {
     public class FizzBuzzTests
     {
-        private static readonly string[] FizzBuzzStrings = ["Fizz", "Buzz", "FizzBuzz"];
+        private readonly FizzBuzz fizzBuzzGame = FizzBuzz.InitGame(FizzBuzzMappings);
+        private static readonly Dictionary<int, string> FizzBuzzMappings = new()
+                                                                           {
+                                                                               { 3, "Fizz" },
+                                                                               { 5, "Buzz" },
+                                                                               { 7, "Whizz" },
+                                                                               { 11, "Bang" }
+                                                                           };
+        private static readonly string[] ValidParts = FizzBuzzMappings.Values.ToArray();
 
         [Theory]
         [InlineData(1, "1")]
-        [InlineData(67, "67")]
-        [InlineData(82, "82")]
         [InlineData(3, "Fizz")]
-        [InlineData(66, "Fizz")]
-        [InlineData(99, "Fizz")]
         [InlineData(5, "Buzz")]
-        [InlineData(50, "Buzz")]
-        [InlineData(85, "Buzz")]
+        [InlineData(7, "Whizz")]
+        [InlineData(11, "Bang")]
         [InlineData(15, "FizzBuzz")]
+        [InlineData(21, "FizzWhizz")]
         [InlineData(30, "FizzBuzz")]
+        [InlineData(33, "FizzBang")]
+        [InlineData(35, "BuzzWhizz")]
         [InlineData(45, "FizzBuzz")]
+        [InlineData(50, "Buzz")]
+        [InlineData(55, "BuzzBang")]
+        [InlineData(66, "FizzBang")]
+        [InlineData(67, "67")]
+        [InlineData(77, "WhizzBang")]
+        [InlineData(82, "82")]
+        [InlineData(85, "Buzz")]
+        [InlineData(99, "FizzBang")]
         public void Returns_Number_Representation(int input, string expectedResult)
-            => Games.FizzBuzz.Convert(input)
+            => fizzBuzzGame.Convert(input)
                 .Should()
                 .BeSome(x => x.Should().Be(expectedResult));
 
@@ -38,17 +53,31 @@ namespace Games.Tests
         private static Arbitrary<int> ValidInput()
             => Gen.Choose(FizzBuzz.Min, FizzBuzz.Max).ToArbitrary();
 
-        private static bool IsConvertValid(int x)
-            => FizzBuzz.Convert(x).Exists(s => ValidStringsFor(x).Contains(s));
+        private bool IsConvertValid(int number)
+        {
+            var resultOption = fizzBuzzGame.Convert(number);
 
-        private static IEnumerable<string> ValidStringsFor(int x)
-            => FizzBuzzStrings.Append(x.ToString());
+            return resultOption.Match(
+                result => IsValidFizzBuzzResult(result, number),
+                () => false
+            );
+        }
+
+        private static bool IsValidFizzBuzzResult(string result, int number)
+        {
+            if (result == number.ToString())
+                return true;
+
+            result = ValidParts.Aggregate(result, (current, part) => current.Replace(part, string.Empty));
+
+            return string.IsNullOrEmpty(result);
+        }
 
         [Property]
         public Property ParseFailForNumbersOutOfRange()
             => Prop.ForAll(
                 InvalidInput(),
-                x => FizzBuzz.Convert(x).IsNone
+                x => fizzBuzzGame.Convert(x).IsNone
             );
 
         private static Arbitrary<int> InvalidInput()
--- exercise/C#/day02/Games/FizzBuzz.cs
+++ exercise/C#/day02/Games/FizzBuzz.cs
@@ -1,32 +1,33 @@
 ﻿using LanguageExt;
 
-namespace Games
+namespace Games;
+
+public class FizzBuzz
 {
-    public static class FizzBuzz
+    public const int Min = 1;
+    public const int Max = 100;
+    private readonly Dictionary<int, string> _mapping;
+
+    private FizzBuzz(Dictionary<int, string> mapping) => _mapping = mapping;
+
+    public static FizzBuzz InitGame(Dictionary<int, string> Mapping) => new(Mapping);
+
+    public  Option<string> Convert(int input) =>
+        IsOutOfRange(input) ? Option<string>.None : ConvertSafely(input);
+
+    private string ConvertSafely(int input)
     {
-        public const int Min = 1;
-        public const int Max = 100;
+        var result = _mapping
+                     .Filter(kvp => IsDivisibleBy(kvp.Key, input))
+                     .Map(kvp => kvp.Value)                           
+                     .Fold(string.Empty, (acc, value) => acc + value);
 
-        private static readonly Dictionary<int, string> Mapping = new()
-            {
-                {15, "FizzBuzz"},
-                {3, "Fizz"},
-                {5, "Buzz"}
-            };
-
-        public static Option<string> Convert(int input)
-            => IsOutOfRange(input)
-                ? Option<string>.None
-                : ConvertSafely(input);
-
-        private static string ConvertSafely(int input)
-            => Mapping
-                .Find(p => Is(p.Key, input))
-                .Map(kvp => kvp.Value)
-                .FirstOrDefault(input.ToString());
-
-        private static bool Is(int divisor, int input) => input % divisor == 0;
-
-        private static bool IsOutOfRange(int input) => input is < Min or > Max;
+        return string.IsNullOrEmpty(result) 
+            ? input.ToString() 
+            : result;
     }
+
+    private static bool IsDivisibleBy(int divisor, int input) => input % divisor == 0;
+
+    private static bool IsOutOfRange(int input) => input is < Min or > Max;
 }
