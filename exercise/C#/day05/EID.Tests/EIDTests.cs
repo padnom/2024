0b8251ee85f63e537e5310c81cf294d8cb8d1c61
padnom <padnomdev@gmail.com>
2024-12-08 18:41:21 +0100

    refactor: update project references and add LanguageExt package for enhanced validation in EIDTests

--- exercise/C#/day05/EID.Tests/EIDTests.cs
+++ exercise/C#/day05/EID.Tests/EIDTests.cs
@@ -1,70 +1,96 @@
+using FluentAssertions.LanguageExt;
+
+using LanguageExt;
+using LanguageExt.Common;
+
 using System.Text.RegularExpressions;
-using FluentAssertions;
+
 using Xunit;
 
-namespace EID.Tests
+using static LanguageExt.Prelude;
+
+namespace EID.Tests;
+
+public class EIDTests
 {
-    public class EIDTests
+    [Fact]
+    public void Empty_String_Is_Not_A_Valid_ID()
     {
-        [Fact]
-        public void Null_Is_Not_A_Valid_ID() => ElfId.Validate(null).Should().BeFalse();
+        ElfId.Validate(string.Empty).Should().BeFail("Value cannot be null or whitespace");
+    }
 
-        [Fact]
-        public void Empty_String_Is_Not_A_Valid_ID() =>
-            ElfId.Validate(string.Empty).Should().BeFalse();
+    [Fact]
+    public void Not_Exactly_8_Characters_Is_Not_A_Valid_ID()
+    {
+        ElfId.Validate("1234567").Should().BeFail("Value does not match pattern");
+    }
 
-        [Fact]
-        public void White_Space_Is_Not_A_Valid_ID() => ElfId.Validate(" ").Should().BeFalse();
+    [Fact]
+    public void Null_Is_Not_A_Valid_ID()
+    {
+        ElfId.Validate(null).Should().BeFail("Value cannot be null or whitespace");
+    }
 
-        [Fact]
-        public void Too_Short_String_Is_Not_A_Valid_ID() => ElfId.Validate("1").Should().BeFalse();
+    [Fact]
+    public void Too_Long_String_Is_A_Valid_ID()
+    {
+        ElfId
+            .Validate(
+                "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+            )
+            .Should()
+            .BeFail("Value does not match pattern");
+    }
 
-        [Fact]
-        public void Too_Long_String_Is_A_Valid_ID()
+    [Fact]
+    public void Too_Short_String_Is_Not_A_Valid_ID()
+    {
+        ElfId.Validate("1").Should().BeFail("Value does not match pattern");
+    }
+
+    [Fact]
+    public void ValidString_Is_A_Valid_ID()
+    {
+        ElfId.Validate("19800767").Should().BeSuccess();
+    }
+
+    [Fact]
+    public void White_Space_Is_Not_A_Valid_ID()
+    {
+        ElfId.Validate(" ").Should().BeFail("Value cannot be null or whitespace");
+    }
+
+    internal class ElfId
+    {
+        internal static Validation<Error, string> Validate(string? value)
         {
-            ElfId
-                .Validate(
-                    "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
-                )
-                .Should()
-                .BeFalse();
+            return ValidateNotNullOrWhitespace(value).Bind(MatchesPattern).Bind(IsValidControlKey);
         }
 
-        [Fact]
-        public void Not_Exactly_8_Characters_Is_Not_A_Valid_ID() =>
-            ElfId.Validate("1234567").Should().BeFalse();
-
-        [Fact]
-        public void ValidString_Is_A_Valid_ID() => ElfId.Validate("19800767").Should().BeTrue();
-
-        internal class ElfId
+        private static Validation<Error, string> IsValidControlKey(string value)
         {
-            internal static bool Validate(string? value)
-            {
-                if (
-                    string.IsNullOrWhiteSpace(value)
-                    || !MatchesPattern(value)
-                    || !IsValidControlKey(value)
-                )
-                {
-                    return false;
-                }
+            var firstSixDigits = int.Parse(value.Substring(0, 6));
+            var controlKey = int.Parse(value.Substring(6, 2));
 
-                return true;
-            }
+            return firstSixDigits % 97 == 97 - controlKey
+                ? Success<Error, string>(value)
+                : Fail<Error, string>(Error.New("Invalid control key"));
+        }
 
-            private static bool MatchesPattern(string value)
-            {
-                string pattern = @"^[1-3][0-9]{2}[0-9]{3}(0[1-9]|[1-8][0-9]|9[0-7])$";
-                return Regex.IsMatch(value, pattern);
-            }
+        private static Validation<Error, string> MatchesPattern(string value)
+        {
+            var pattern = @"^[1-3][0-9]{2}[0-9]{3}(0[1-9]|[1-8][0-9]|9[0-7])$";
 
-            private static bool IsValidControlKey(string value)
-            {
-                int firstSixDigits = int.Parse(value.Substring(0, 6));
-                int controlKey = int.Parse(value.Substring(6, 2));
-                return firstSixDigits % 97 == 97 - controlKey;
-            }
+            return Regex.IsMatch(value, pattern)
+                ? Success<Error, string>(value)
+                : Fail<Error, string>(Error.New("Invalid pattern"));
+        }
+
+        private static Validation<Error, string> ValidateNotNullOrWhitespace(string? value)
+        {
+            return string.IsNullOrWhiteSpace(value)
+                ? Fail<Error, string>(Error.New("Value cannot be null or whitespace"))
+                : Success<Error, string>(value);
         }
     }
 }
