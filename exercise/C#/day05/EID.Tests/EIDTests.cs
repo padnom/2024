d2f2079d79201b67f16c44b15edc9d7e268c7872
padnom <padnomdev@gmail.com>
2024-12-08 17:37:14 +0100

    refactor: rename EilfId to ElfId in EIDTests and update related test cases

--- exercise/C#/day05/EID.Tests/EIDTests.cs
+++ exercise/C#/day05/EID.Tests/EIDTests.cs
@@ -6,22 +6,22 @@ namespace EID.Tests
     public class EIDTests
     {
         [Fact]
-        public void Null_Is_Not_A_Valid_ID() => EilfId.Validate(null).Should().BeFalse();
+        public void Null_Is_Not_A_Valid_ID() => ElfId.Validate(null).Should().BeFalse();
 
         [Fact]
         public void Empty_String_Is_Not_A_Valid_ID() =>
-            EilfId.Validate(string.Empty).Should().BeFalse();
+            ElfId.Validate(string.Empty).Should().BeFalse();
 
         [Fact]
-        public void White_Space_Is_Not_A_Valid_ID() => EilfId.Validate(" ").Should().BeFalse();
+        public void White_Space_Is_Not_A_Valid_ID() => ElfId.Validate(" ").Should().BeFalse();
 
         [Fact]
-        public void Too_Short_String_Is_Not_A_Valid_ID() => EilfId.Validate("1").Should().BeFalse();
+        public void Too_Short_String_Is_Not_A_Valid_ID() => ElfId.Validate("1").Should().BeFalse();
 
         [Fact]
         public void Too_Long_String_Is_A_Valid_ID()
         {
-            EilfId
+            ElfId
                 .Validate(
                     "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
                 )
@@ -31,9 +31,9 @@ namespace EID.Tests
 
         [Fact]
         public void Not_Exactly_8_Characters_Is_Not_A_Valid_ID() =>
-            EilfId.Validate("1234567").Should().BeFalse();
+            ElfId.Validate("1234567").Should().BeFalse();
 
-        internal class EilfId
+        internal class ElfId
         {
             internal static bool Validate(string? value)
             {
